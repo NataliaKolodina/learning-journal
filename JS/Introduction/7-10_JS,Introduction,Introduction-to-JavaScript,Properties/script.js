8d2bb00581a455260e563cc0c83136c5c7469989
Nataly <kolodina_melbyk@mail.ru>
2025-05-19 20:52:08 +0400

    add ex 7-10_JS,Introduction,Introduction-to-JavaScript,Properties

---
+++ JS/Introduction/7-10_JS,Introduction,Introduction-to-JavaScript,Properties/script.js
@@ -0,0 +1,7 @@
+// Stampa la lunghezza di una stringa
+console.log('Programmare è divertente!'.length);
+
+// Stampa la lunghezza della stringa 'JavaScript' (ci si aspetta 10)
+console.log('JavaScript'.length);
+// Stampa la lunghezza della stringa 'JS' (ci si aspetta 2)
+console.log('JS'.length);
