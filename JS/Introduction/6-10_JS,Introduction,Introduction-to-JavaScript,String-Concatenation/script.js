0643615f7a243e9ba94c3244b16c82c77627814d
Nataly <kolodina_melbyk@mail.ru>
2025-05-19 20:43:49 +0400

    add ex 6-10_JS,Introduction,Introduction-to-JavaScript,String-Concatenation

---
+++ JS/Introduction/6-10_JS,Introduction,Introduction-to-JavaScript,String-Concatenation/script.js
@@ -0,0 +1,8 @@
+// concatenazione delle seguenti due stringhe: 'Ciao', ' mondo'
+console.log('Ciao' + ' mondo');
+// 'Imparo' concatenato con ' JavaScript'
+console.log('Imparo' + ' JavaScript');
+// concateni e stampi le seguenti stringhe: 'Studio' + ' ' + 'JavaScript' + ' ' + 'ogni giorno!'
+console.log('Studio' + ' ' + 'JavaScript' + ' ' + 'ogni giorno!');
+// 'Pane', 'burro', 'e', 'marmellata'
+console.log('Pane' + ' ' + 'burro' + ' ' + 'e' + ' ' + 'marmellata');
