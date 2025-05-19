a1acc06cebd154c9ebe0ab144fbf77fc4f2fde0e
Nataly <kolodina_melbyk@mail.ru>
2025-05-19 20:07:51 +0400

    add ex 5-10_JS,Introduction,Introduction-to-JavaScript,Arithmetic-Operators

---
+++ JS/Introduction/5-10_JS,Introduction,Introduction-to-JavaScript,Arithmetic-Operators/script.js
@@ -0,0 +1,11 @@
+console.log(8 + 12);
+console.log(20 - 5);
+console.log(7 * 3);
+console.log(36 / 6);
+
+// resto della divisione tra 29 e 4
+console.log(29 % 4);
+
+// resto tra 15 e 5
+console.log(15 % 5);
+console.log('3 + 4');
