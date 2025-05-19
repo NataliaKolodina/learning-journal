5182053fbaa7acec61397d8c2cb8a78a245f1272
Nataly <kolodina_melbyk@mail.ru>
2025-05-19 22:54:46 +0400

    change ex 8-10_JS,Introduction,Introduction-to-JavaScript,Methods

--- JS/Introduction/8-10_JS,Introduction,Introduction-to-JavaScript,Methods/script.js
+++ JS/Introduction/8-10_JS,Introduction,Introduction-to-JavaScript,Methods/script.js
@@ -1,4 +1,6 @@
 //  Usa un metodo per trasformare una stringa in maiuscolo
-console.log('imparare javascript è utile'.toUpperCase());
+console.log("imparare javascript è utile".toUpperCase());
 // Verifica l’inizio di una stringa con un metodo
-console.log('Programmazione'.startsWith('P'));
+console.log("Programmazione".startsWith("P"));
+
+
