7bda66d883484b16b0631290d677d3ba7f18b72a
Nataly <kolodina_melbyk@mail.ru>
2025-05-19 19:55:59 +0400

    add ex 4-10_JS,Introduction,Introduction-to-JavaScript,Data-Types

---
+++ JS/Introduction/4-10_JS,Introduction,Introduction-to-JavaScript,Data-Types/script.js
@@ -0,0 +1,18 @@
+console.log('La sede centrale di Codecademy si trova a New York');
+console.log(2025);
+
+// dati primitivi
+//  numero con decimali
+console.log(12.34);
+// stringa con lettere, numeri e simboli
+console.log('abc123!@#');
+// booleano
+console.log(true);
+// booleano
+console.log(false);
+// null
+console.log(null);
+// undefined
+console.log(undefined);
+// BigInt
+console.log(1234567890123456n);
