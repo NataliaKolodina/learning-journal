527807cbbd95065b5b4b753bf6d8fa76fc86f14d
Nataly <kolodina_melbyk@mail.ru>
2025-05-21 21:12:24 +0400

    add ex 9-10_JS,Introduction,Variables,typeof-operator

---
+++ JS/Introduction/Variables/9-10_JS,Introduction,Variables,typeof-operator/script.js
@@ -0,0 +1,9 @@
+const animal = 'gatto';
+console.log(typeof animal);
+
+const numeroScarpe = 42;
+const haAnimaleDomestico = true;
+const città = 'Roma';
+console.log(typeof numeroScarpe);
+console.log(typeof haAnimaleDomestico);
+console.log(typeof città);
