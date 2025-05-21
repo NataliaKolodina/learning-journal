9942e9c42dc16db5be126e342f2b989a030386d0
Nataly <kolodina_melbyk@mail.ru>
2025-05-21 13:22:03 +0400

    add ex 10-10_JS,Introduction,Introduction-to-JavaScript,Review

---
+++ JS/Introduction/10-10_JS,Introduction,Introduction-to-JavaScript,Review/script.js
@@ -0,0 +1,14 @@
+// Questo programma mostra la lunghezza di una stringa in maiuscolo
+console.log('Benvenuto nel mondo JavaScript');
+console.log('Benvenuto nel mondo JavaScript'.length);
+console.log('Benvenuto nel mondo JavaScript'.toUpperCase());
+
+/* Questo esercizio esegue operazioni aritmetiche
+ e mostra anche l’uso degli oggetti e metodi */
+ console.log(24 / 3 * 2);
+ console.log('Il risultato è: ');
+ console.log('Il risultato è: ' + (24 / 3 * 2));
+
+
+ console.log(Math.random() * 10);
+ console.log('Ciao'.startsWith('C'));
