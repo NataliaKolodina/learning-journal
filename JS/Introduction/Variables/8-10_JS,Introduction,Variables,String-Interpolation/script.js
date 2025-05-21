57dac3feacc6ec706edf7ee2bd3ab7e0d09e23fc
Nataly <kolodina_melbyk@mail.ru>
2025-05-21 20:55:06 +0400

    add ex 8-10_JS,Introduction,Variables,String-Interpolation

---
+++ JS/Introduction/Variables/8-10_JS,Introduction,Variables,String-Interpolation/script.js
@@ -0,0 +1,6 @@
+const fruttoPreferito = 'fragola';
+console.log(` Il mio frutto preferito è la ${fruttoPreferito}.`);
+
+const nomeUtente = 'Marco';
+const ciboPreferito = 'pizza';
+console.log( `Ciao, mi chiamo ${nomeUtente} e adoro la ${ciboPreferito}!`);
