5a4b647ee7ecccbdb202d2d974a32efb7c03ea75
Nataly <kolodina_melbyk@mail.ru>
2025-05-21 13:15:35 +0400

    add ex 9-10_JS,Introduction,Introduction-to-JavaScript,Built-in-Objects

---
+++ JS/Introduction/9-10_JS,Introduction,Introduction-to-JavaScript,Built-in Objects/script.js
@@ -0,0 +1,7 @@
+// Questo codice genera un numero casuale con decimali
+console.log(Math.random());
+
+
+// Stampa un numero casuale intero tra 0 e 50
+console.log(Math.floor(Math.random() * 50));
+
