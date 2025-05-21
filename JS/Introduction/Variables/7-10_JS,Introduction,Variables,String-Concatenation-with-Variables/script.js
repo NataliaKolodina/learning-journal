b805221cb3b768437673b92f5cf3bfc42231cb69
Nataly <kolodina_melbyk@mail.ru>
2025-05-21 20:45:13 +0400

    add ex 7-10_JS,Introduction,Variables,String-Concatenation-with-Variables

---
+++ JS/Introduction/Variables/7-10_JS,Introduction,Variables,String-Concatenation-with-Variables/script.js
@@ -0,0 +1,6 @@
+let animal = 'coniglio';
+console.log('Possiedo un animale domestico: ' + animal);
+
+let nome = 'Luca';
+let hobby = 'giocare a calcio';
+console.log('Il mio nome è ' + nome + ' e mi piace ' + hobby);
