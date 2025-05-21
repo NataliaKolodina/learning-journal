7f2eeefe717578f157a5a6da9fddc8c30c2ed7bc
Nataly <kolodina_melbyk@mail.ru>
2025-05-21 19:01:15 +0400

    add ex 1-10_JS,Introduction,Variables,Variable-Intro

---
+++ JS/Introduction/Variables/1-10_JS,Introduction,Variables,Variable-Intro/script.js
@@ -0,0 +1,9 @@
+let nomeUtente = 'Marco';
+let numeroConto = 123456;
+let messaggioSaluto = 'Ciao, Marco!';
+
+var colorePreferito = 'blu';
+const codiceFiscale = 'RSSMRC80A01H501U';
+colorePreferito = 'verde';
+console.log(colorePreferito);
+console.log(messaggioSaluto);
