7bae9208c6bf36bcdb182b1089b81ba489591fd7
Nataly <kolodina_melbyk@mail.ru>
2025-05-21 21:24:54 +0400

    add ex 10-10_JS,Introduction,Variables,Review-Variables

---
+++ JS/Introduction/Variables/10-10_JS,Introduction,Variables,Review-Variables/script.js
@@ -0,0 +1,18 @@
+let ciboPreferito = 'lasagna';
+let prezzo;
+console.log(prezzo); 
+prezzo = 10;
+console.log(`Il prezzo della ${ciboPreferito} è di ${prezzo} euro.`);
+
+const ristorante = 'Trattoria Bella';
+console.log(ristorante + ' serve il miglior piatto di lasagna!');
+
+let punteggio = 15;
+punteggio += 5;
+punteggio -= 2;
+punteggio++;
+let tipoPunteggio = typeof punteggio;
+console.log(`Il tipo del punteggio è: ${tipoPunteggio}`);
+
+var valoreBooleano = true;
+console.log(typeof valoreBooleano);
