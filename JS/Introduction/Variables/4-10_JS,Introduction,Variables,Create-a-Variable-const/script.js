a0a08f5c2ffc376b7eb2e44acbb853cb88e6ce7d
Nataly <kolodina_melbyk@mail.ru>
2025-05-21 19:54:46 +0400

    add ex 4-10_JS,Introduction,Variables,Create-a-Variable-const

---
+++ JS/Introduction/Variables/4-10_JS,Introduction,Variables,Create-a-Variable-const/script.js
@@ -0,0 +1,8 @@
+// Questa variabile non può essere modificata
+const nomeUtente = 'Luca';
+console.log(nomeUtente);
+
+// Le costanti devono avere un valore assegnato subito
+// const giornoSettimana; // Questa riga genera errore
+// console.log(giornoSettimana); // Questa riga non viene eseguita
+// Errore: una costante deve essere inizializzata
