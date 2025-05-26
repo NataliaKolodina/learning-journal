a271bf8148b734ddc4a5a0a0927d2c569ae932e9
Nataly <kolodina_melbyk@mail.ru>
2025-05-26 15:40:54 +0400

    add ex 2-11_JS,Conditional-Statements,If-Statement

---
+++ JS/Conditional/Conditional Statements/2-11_JS,Conditional-Statements,If-Statement/script.js
@@ -0,0 +1,20 @@
+let weatherNice = true;
+// weatherNice = false;
+if (weatherNice) {
+  console.log("Let's go outside!");
+}
+
+let alarmRings = true;
+if (alarmRings) {
+  console.log("Time to wake up!");
+}
+
+let isTired = false;
+if (isTired) {
+  console.log("Go to sleep!");
+}
+
+let hasHomework = true;
+if (hasHomework) {
+  console.log("Do your homework now.");
+}
