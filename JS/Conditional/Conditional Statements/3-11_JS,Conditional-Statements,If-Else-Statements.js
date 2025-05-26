892083f3050fee58e8c52715de75a724e60bc289
Nataly <kolodina_melbyk@mail.ru>
2025-05-26 15:45:43 +0400

    add file

---
+++ JS/Conditional/Conditional Statements/3-11_JS,Conditional-Statements,If-Else-Statements.js
@@ -0,0 +1,28 @@
+// 3-11_JS,Conditional-Statements,If-Else-Statements
+/* ======================================
+ 🔸 EXERCISE 1: Eseguire un’azione con if o con else
+ Scrivi un blocco if...else per eseguire un comportamento automatico basato su una condizione binaria.
+Crea una variabile chiamata isLoggedIn.
+Assegna a isLoggedIn il valore true.
+Scrivi una struttura if...else che controlla il valore di isLoggedIn.
+Se la condizione è vera, stampa esattamente:
+ "Welcome back!"
+Altrimenti, stampa esattamente:
+ "Please log in."
+Salva le modifiche ed esegui il codice per vedere quale messaggio appare.
+Non modificare il valore della variabile una volta dichiarato.
+====================================== */
+/* ======================================
+ 🔸 EXERCISE 2: Eseguire l’effetto opposto con if...else
+ Questo esercizio applica la stessa struttura condizionale, ma con un significato inverso rispetto al primo esercizio.
+Crea una variabile chiamata isSubscribed.
+Assegna a isSubscribed il valore false.
+Scrivi una struttura if...else che controlla il valore di isSubscribed.
+Se la condizione è vera, stampa esattamente:
+ "Enjoy your premium content."
+Altrimenti, stampa esattamente:
+ "Subscribe to access this content."
+Salva le modifiche ed esegui il codice per verificare quale messaggio appare.
+Non modificare manualmente il valore assegnato alla variabile.
+====================================== */
+
