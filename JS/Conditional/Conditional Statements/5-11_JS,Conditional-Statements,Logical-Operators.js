4a24fde45ff7f7bf25e22a74e2eec03a1fa09922
Nataly <kolodina_melbyk@mail.ru>
2025-05-26 20:45:23 +0400

    add ex 5-11_JS,Conditional-Statements,Logical-Operators

--- JS/Conditional/Conditional Statements/5-11_JS,Conditional-Statements,Logical-Operators.js
+++ JS/Conditional/Conditional Statements/5-11_JS,Conditional-Statements,Logical-Operators.js
@@ -44,3 +44,29 @@ Risultati attesi:
 // "The item is currently unavailable."
 // "Weekend mode activated."
 ====================================== */
+
+const temperature = 22;
+const isRaining = false;
+if (temperature >= 20 && isRaining === false) {
+  console.log("Perfect day for a walk.");
+} else {
+  console.log("Maybe stay inside.");
+}
+if (temperature > 30 || isRaining === true) {
+  console.log("Avoid going out now.");
+} else {
+  console.log("The weather is manageable.");
+}
+
+const isAvailable = false;
+if (!isAvailable) {
+  console.log("The item is currently unavailable.");
+} else {
+  console.log("Item in stock.");
+}
+const isWeekend = true;
+if (!isWeekend) {
+  console.log("It's a weekday.");
+} else {
+  console.log("Weekend mode activated.");
+}
