2a57bb18b6e42599f44112a6fc71c0c2126564c3
Nataly <kolodina_melbyk@mail.ru>
2025-05-28 20:30:41 +0400

    add ex 10-11_JS,Conditional-Statements,The-switch-keyword

--- JS/Conditional/Conditional Statements/10-11_JS,Conditional-Statements,The-switch-keyword.js
+++ JS/Conditional/Conditional Statements/10-11_JS,Conditional-Statements,The-switch-keyword.js
@@ -21,3 +21,35 @@ Aggiungi un caso default che stampi "Just a regular day" // Risultato atteso: (n
 Usa break alla fine di ogni blocco case.
 Esegui il codice per assicurarti che venga stampato "Rest and recharge".
  ====================================== */
+
+let productName = "lime";
+switch (productName) {
+  case "tomato":
+    console.log("Tomatoes are $0.49");
+    break;
+  case "lime":
+    console.log("Limes are $1.49");
+    break;
+  case "papaya":
+    console.log("Papayas are $1.29");
+    break;
+  default:
+    console.log("Invalid item");
+    break;
+}
+
+let currentDay = "Sunday";
+switch (currentDay) {
+  case "Monday":
+    console.log("Start of the work week");
+    break;
+  case "Friday":
+    console.log("Almost the weekend!");
+    break;
+  case "Sunday":
+    console.log("Rest and recharge");
+    break;
+  default:
+    console.log("Just a regular day");
+    break;
+}
