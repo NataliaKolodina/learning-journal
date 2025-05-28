f8f06ad5c129d7f7a3d3dd9152d23368d599da48
Nataly <kolodina_melbyk@mail.ru>
2025-05-28 20:07:59 +0400

    add file

---
+++ JS/Conditional/Conditional Statements/8-11_JS,Conditional-Statements,Ternary-Operator.js
@@ -0,0 +1,20 @@
+// 8-11_JS,Conditional-Statements,Ternary-Operator
+/* ======================================
+ 🔸 EXERCISE 1: Convertire un if...else in un operatore ternario
+Crea una variabile isOnline e assegnale il valore false.
+Scrivi una dichiarazione if...else che controlla isOnline:
+Se il valore è true, stampa "User is active" usando console.log.
+Altrimenti, stampa "User is offline".
+Commenta l’intero blocco if...else con //.
+Sotto il commento, riscrivi la stessa logica utilizzando un operatore ternario.
+Salva le modifiche, esegui il codice e verifica il risultato. // Risultato atteso: User is offline
+====================================== */
+/* ======================================
+ 🔸 EXERCISE 2: Eseguire un controllo con ternario su una stringa
+
+Crea una variabile userRole e assegnale la stringa 'guest'.
+Utilizza un operatore ternario per controllare se userRole è uguale a 'admin':
+Se vero, stampa "Access granted" usando console.log.
+Se falso, stampa "Access denied".
+Salva le modifiche, esegui il codice e verifica il risultato. // Risultato atteso: Access denied
+====================================== */
