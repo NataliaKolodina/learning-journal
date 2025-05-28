0e083cf13527dc88d39ad55e74fcc07f4a24795e
Nataly <kolodina_melbyk@mail.ru>
2025-05-28 20:35:13 +0400

    add file

---
+++ JS/Conditional/Conditional Statements/11-11_JS,Conditional-Statements,Review.js
@@ -0,0 +1,25 @@
+// 11-11_JS,Conditional-Statements,Review
+/* ======================================
+ 🔸 EXERCISE 1: Creare condizioni con if, else if ed else
+All’interno dello script:
+Crea una variabile temperature e assegnale il valore 22.
+Scrivi una condizione if che controlli se temperature è maggiore di 30. Se sì, imposta il contenuto del div result su: "It’s hot today." // Risultato atteso: (non verrà stampato)
+Aggiungi un ramo else if che controlla se temperature è minore di 15. In quel caso, imposta il contenuto su: "It’s cold today." // Risultato atteso: (non verrà stampato)
+In tutti gli altri casi (else), imposta il contenuto su: "The temperature is moderate." // Risultato atteso: The temperature is moderate.
+Usa l’operatore && per creare una condizione in un secondo blocco if:
+Se temperature è maggiore o uguale a 20 e minore o uguale a 25, imposta un console.log() con il messaggio: "Perfect for a walk!"
+Usa l’operatore || in un’altra condizione if:  // Risultato atteso: Perfect for a walk!
+Se temperature è minore di 10 oppure maggiore di 35, stampa: "Extreme weather alert!"  // Risultato atteso: (non verrà stampato)
+Esegui il codice per verificare che venga stampato "The temperature is moderate. Perfect for a walk!".
+====================================== */
+/* ======================================
+ 🔸 EXERCISE 2: Determinare l’azione del semaforo
+Crea una variabile lightColor e assegnale il valore 'green'.
+Usa una dichiarazione switch per determinare l’azione corretta:
+Se lightColor è 'red', stampa "Stop!" // Risultato atteso: (non verrà stampato)
+Se lightColor è 'yellow', stampa "Slow down." // Risultato atteso: (non verrà stampato)
+Se lightColor è 'green', stampa "Go!" // Risultato atteso: Go!
+Usa default per stampare "Caution, unknown color." // Risultato atteso: (non verrà stampato)
+Assicurati di usare break dopo ogni case.
+Esegui il codice per verificare che venga stampato "Go!".
+====================================== */
