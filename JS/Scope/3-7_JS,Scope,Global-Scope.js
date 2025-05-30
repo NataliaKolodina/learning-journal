32e3e8b77b702db3fadfd42fd05fba15af48e6c3
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 22:40:02 +0400

    Create 3-7_JS,Scope,Global-Scope.js

---
+++ JS/Scope/3-7_JS,Scope,Global-Scope.js
@@ -0,0 +1,22 @@
+// 3-7_JS,Scope,Global-Scope
+/* ======================================
+🔸 ESERCIZIO 1: Utilizzare una variabile globale dentro una funzione
+1. Dichiara una costante chiamata `planet` e assegnale la stringa `"Earth"`.
+2. Crea una funzione con il nome `logPlanet`.
+3. All’interno della funzione `logPlanet`, stampa il valore della variabile `planet` nella console. // Risultato atteso: Earth
+4. Chiama `logPlanet`.
+5. Verifica i risultati nella console.
+====================================== */
+/* ======================================
+🔸 ESERCIZIO 2: Accedere a una variabile globale da diversi blocchi
+1. Dichiara una variabile `language` con il valore `"JavaScript"` al di fuori di qualsiasi funzione o blocco `if`.
+2. Crea una funzione chiamata `showLanguage`.
+3. All’interno della funzione `showLanguage`, stampa la variabile `language` nella console. // Risultato atteso: JavaScript
+4. Chiama la funzione `showLanguage`.
+5. Crea un blocco `if` che verifica se `language` è uguale a `"JavaScript"`.
+6. All’interno del blocco `if`, stampa di nuovo la variabile `language` nella console. // Risultato atteso: JavaScript
+7. Verifica i risultati nella console.
+====================================== */
+
+
+
