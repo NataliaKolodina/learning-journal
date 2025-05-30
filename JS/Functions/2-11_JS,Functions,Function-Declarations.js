040cf8ac7ae910ed3db9ef3101eb2422be4abaa2
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 14:27:35 +0400

    create file 2-11_JS,Functions,Function-Declarations

---
+++ JS/Functions/2-11_JS,Functions,Function-Declarations.js
@@ -0,0 +1,18 @@
+// 2-11_JS,Functions,Function-Declarations
+
+/* ======================================
+EXERCISE 1: Dichiarare e chiamare una funzione
+1. Crea una funzione con nome `sayHello` usando la dichiarazione di funzione.
+2. All’interno del corpo della funzione, stampa nel terminale la stringa: "Ciao!"
+3. Dopo aver definito la funzione, chiamala una volta per eseguire il codice. // Ciao!
+4. Subito dopo, chiamala una seconda volta per vedere che può essere eseguita più volte. // Ciao!
+====================================== */
+/* ======================================
+EXERCISE 2: Creare più funzioni e richiamarle
+1. Crea una funzione chiamata `startDay` che stampa nel terminale: "Buona giornata!"
+2. Crea una seconda funzione chiamata `endDay` che stampa: "Buona notte!"
+3. Prima chiama la funzione `startDay`, poi chiama la funzione `endDay`.
+   // Buona giornata!
+   // Buona notte!
+====================================== */
+
