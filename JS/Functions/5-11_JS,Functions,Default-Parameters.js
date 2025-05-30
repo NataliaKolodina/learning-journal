390fe2db205469b966f2bc910824b366125ec3ea
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 15:32:11 +0400

    add ex 5-11_JS,Functions,Default-Parameters

--- JS/Functions/5-11_JS,Functions,Default-Parameters.js
+++ JS/Functions/5-11_JS,Functions,Default-Parameters.js
@@ -25,3 +25,16 @@ EXERCISE 2: Applicare valori predefiniti a più parametri
    // bread
    // eggs
 ====================================== */
+
+function introducePerson(name = "guest") {
+  console.log(`Benvenuto, ${name}!`);
+}
+introducePerson("Giulia");
+
+function makeShoppingList(item1 = "milk", item2 = "bread", item3 = "eggs") {
+  console.log(item1);
+  console.log(item2);
+  console.log(item3);
+}
+makeShoppingList("pasta", "cheese");
+makeShoppingList();
