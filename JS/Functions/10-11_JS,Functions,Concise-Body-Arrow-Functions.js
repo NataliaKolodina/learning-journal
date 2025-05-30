9df510e247eb96ac4e1e4abb85a7032fce5ab509
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 19:38:42 +0400

    add ex 10-11_JS,Functions,Concise-Body-Arrow-Functions

--- JS/Functions/10-11_JS,Functions,Concise-Body-Arrow-Functions.js
+++ JS/Functions/10-11_JS,Functions,Concise-Body-Arrow-Functions.js
@@ -24,3 +24,12 @@ ESERCIZIO 2: Convertire i gradi Celsius in Fahrenheit con funzioni di supporto
    // Output previsto: 77
 5. Salvare le modifiche ed eseguire il codice per verificare il risultato.
 ====================================== */
+
+const calculateRectangleArea = (width, height) => width * height;
+const rectangleAreaResult = calculateRectangleArea(4, 6);
+console.log(rectangleAreaResult);
+
+const multiplyByNineFifths = (number) => number * (9 / 5);
+const getFahrenheit = (celsius) => multiplyByNineFifths(celsius) + 32;
+const fahrenheitResult = getFahrenheit(25);
+console.log(fahrenheitResult);
