8c3f2445aebb8860ccbff13615573622859ab86e
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 15:09:53 +0400

    create file 4-11_JS,Functions,Parameters-and-Arguments

---
+++ JS/Functions/4-11_JS,Functions,Parameters-and-Arguments.js
@@ -0,0 +1,17 @@
+// 4-11_JS,Functions,Parameters-and-Arguments
+/* ======================================
+EXERCISE 1: Creare una funzione con parametri
+1. Dichiarare una funzione chiamata `printFullName` con due parametri: `firstName` e `lastName`.
+2. All’interno del corpo della funzione, stampa nel terminale la frase: "Il tuo nome completo è: " seguita dai due parametri concatenati con uno spazio tra di loro.
+3. Chiama la funzione e passa le stringhe "Luca" e "Rossi" come argomenti.
+   // Il tuo nome completo è: Luca Rossi
+====================================== */
+/* ======================================
+EXERCISE 2: Utilizzare parametri numerici per un calcolo
+1. Crea una funzione chiamata `calculateTotal` con due parametri: `price` e `quantity`.
+2. All’interno della funzione, stampa il risultato della moltiplicazione tra `price` e `quantity`.
+3. Chiama la funzione e passa i numeri 4.5 e 3 come argomenti.
+   // 13.5
+4. Subito dopo, chiama la funzione di nuovo con i valori 10 e 2.
+   // 20
+====================================== */
