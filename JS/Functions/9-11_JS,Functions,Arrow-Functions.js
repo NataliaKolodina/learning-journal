ca287b1637e8815995ac0584dac33dcb21aa8703
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 18:36:28 +0400

    Create 9-11_JS,Functions,Arrow-Functions.js

---
+++ JS/Functions/9-11_JS,Functions,Arrow-Functions.js
@@ -0,0 +1,17 @@
+// 9-11_JS,Functions,Arrow-Functions
+/* ======================================
+EXERCISE 5: Arrow Function
+1. Crea una funzione freccia chiamata `multiply`.
+2. La funzione deve accettare due parametri `a` e `b`.
+3. Deve restituire il prodotto di `a` e `b`.
+4. Stampa nella console il risultato della chiamata a `multiply(4, 5)`.
+   // 20
+====================================== */
+/* ======================================
+EXERCISE 6: Arrow Function abbreviata
+1. Crea una funzione freccia abbreviata chiamata `half`.
+2. La funzione accetta un parametro `number`.
+3. Deve restituire `number / 2` in una sola riga senza `{}` o `return`.
+4. Stampa nella console il risultato della chiamata a `half(10)`.
+   // 5
+====================================== */
