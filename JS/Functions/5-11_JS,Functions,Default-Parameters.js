66cf6674d916ea59d1c4510b65b57c06712e253d
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 15:26:44 +0400

    Create 5-11_JS,Functions,Default-Parameters.js

---
+++ JS/Functions/5-11_JS,Functions,Default-Parameters.js
@@ -0,0 +1,27 @@
+// 5-11_JS,Functions,Default-Parameters
+/* ======================================
+EXERCISE 1: Usare parametri con valori predefiniti
+1. Crea una funzione chiamata `introducePerson` con un parametro `name` che ha un valore predefinito di `'guest'`.
+2. All’interno del corpo della funzione, stampa la frase: "Benvenuto, [name]!".
+3. Chiama la funzione passando l’argomento `'Giulia'`.
+   // Benvenuto, Giulia!
+4. Chiama la funzione di nuovo senza passare nessun argomento.
+   // Benvenuto, guest!
+====================================== */
+/* ======================================
+EXERCISE 2: Applicare valori predefiniti a più parametri
+1. Crea una funzione chiamata `makeShoppingList` con tre parametri: `item1`, `item2`, `item3`.
+2. Imposta i valori predefiniti dei parametri come segue:
+   - `item1` = `'milk'`
+   - `item2` = `'bread'`
+   - `item3` = `'eggs'`
+3. All’interno del corpo della funzione, stampa ciascun parametro su una nuova riga usando `console.log`.
+4. Chiama la funzione passando `'pasta'` e `'cheese'` come primi due argomenti.
+   // pasta
+   // cheese
+   // eggs
+5. Chiama la funzione di nuovo senza passare alcun argomento.
+   // milk
+   // bread
+   // eggs
+====================================== */
