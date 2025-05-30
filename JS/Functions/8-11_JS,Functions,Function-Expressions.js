fbafde086c8f85b4499d8279495639432c5ff258
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 16:16:40 +0400

    add ex 8-11_JS,Functions,Function-Expressions

--- JS/Functions/8-11_JS,Functions,Function-Expressions.js
+++ JS/Functions/8-11_JS,Functions,Function-Expressions.js
@@ -16,3 +16,14 @@ EXERCISE 2: Sommare due numeri usando un'espressione di funzione
 5. Stampa `total` nella console.
    // 17
 ====================================== */
+
+const greetUser = function (name) {
+  console.log(`Welcome, ${name}!`);
+};
+greetUser("Marco");
+
+const addNumbers = function (x, y) {
+  return x + y;
+};
+const total = addNumbers(5, 12);
+console.log(total);
