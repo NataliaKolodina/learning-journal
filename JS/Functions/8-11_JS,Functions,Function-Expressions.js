15ba23b9ecb9c60aea6c6619f2eeb10296d76025
Nataly <kolodina_melbyk@mail.ru>
2025-05-30 16:14:03 +0400

    Create 8-11_JS,Functions,Function-Expressions.js

---
+++ JS/Functions/8-11_JS,Functions,Function-Expressions.js
@@ -0,0 +1,18 @@
+// 8-11_JS,Functions,Function-Expressions
+/* ======================================
+EXERCISE 1: Definire e usare un'espressione di funzione
+1. Crea una costante chiamata `greetUser`.
+2. Assegna a `greetUser` una funzione anonima che prende un parametro `name`.
+3. All’interno della funzione, stampa nella console il messaggio: "Welcome, [nome]!"
+4. Chiama la funzione `greetUser` passando l’argomento "Marco".
+   // Welcome, Marco!
+====================================== */
+/* ======================================
+EXERCISE 2: Sommare due numeri usando un'espressione di funzione
+1. Crea una costante chiamata `addNumbers`.
+2. Assegna a `addNumbers` una funzione anonima che prende due parametri: `x` e `y`.
+3. La funzione deve restituire la somma di `x` e `y`.
+4. Crea una costante `total` e assegnale il valore restituito dalla chiamata a `addNumbers` con `5` e `12`.
+5. Stampa `total` nella console.
+   // 17
+====================================== */
