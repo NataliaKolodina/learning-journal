d04166f1522a31f94d8295e5493e3641bf6972ac
Nataly <kolodina_melbyk@mail.ru>
2025-06-02 21:37:56 +0400

    Create 4-12_JS,Arrays,Update-Elements.js

---
+++ JS/Arrays/4-12_JS,Arrays,Update-Elements.js
@@ -0,0 +1,29 @@
+// 4-12_JS,Arrays,Update-Elements
+
+/* ======================================
+🔸 Esercizio 1: Aggiornare il valore di un elemento in un array
+1. Crea una variabile chiamata `months` e assegnale un array con queste stringhe esatte e in quest’ordine:
+   'January', 'February', 'March', 'April'.
+2. Aggiorna l’elemento all’indice 2 con la stringa 'March-April' (unisci i due mesi in una sola stringa).
+3. Stampa l’intero array `months` nella console.
+   // Output atteso: ['January', 'February', 'March-April', 'April']
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per controllare l’output.
+====================================== */
+
+/* ======================================
+🔸 Esercizio 2: Modificare un carattere in una stringa dentro un array
+1. Crea una variabile chiamata `words` e assegnale un array contenente la stringa 'Hello'.
+2. Poiché le stringhe sono immutabili, converti la stringa all’indice 0 in un array di caratteri.
+3. Cambia il carattere all’indice 1 (la seconda lettera) con la lettera 'a'.
+4. Converti l’array di caratteri di nuovo in una stringa.
+5. Aggiorna l’elemento all’indice 0 dell’array `words` con questa nuova stringa.
+6. Stampa l’array `words` aggiornato nella console.
+   // Output atteso: ['Hallo']
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per controllare l’output.
+====================================== */
+
+
+
+
