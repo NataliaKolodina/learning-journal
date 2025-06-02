6db79f4e6cbb700a6f8fc69dd41ed3bc6a6f1c3b
Nataly <kolodina_melbyk@mail.ru>
2025-06-02 20:56:17 +0400

    add ex 3-12_JS,Arrays,Accessing-Elements

--- JS/Arrays/3-12_JS,Arrays,Accessing-Elements.js
+++ JS/Arrays/3-12_JS,Arrays,Accessing-Elements.js
@@ -6,16 +6,23 @@
 3. Accedi all'ultimo elemento dell'array `cities` e memorizzalo in una variabile chiamata `lastCity`.  
 4. Stampa `firstCity` nella console (dovrebbe stampare "Rome").  
 5. Stampa `lastCity` nella console (dovrebbe stampare "Berlin").  
-
 Salva le modifiche, aggiorna la pagina ed esegui il codice per vedere l'output.  
 ====================================== */
-
 /* ======================================  
-🔸 ESERCIZIO 2: Accedi ai caratteri in una stringa da elementi di un array  
-1. Crea una variabile chiamata `greetings` e assegnale un array con questa stringa esattamente: 'Hello World'.  
-2. Accedi all'elemento all'indice 0 dell'array `greetings` e memorizzalo in una variabile chiamata `helloString`.  
-3. Accedi al carattere all'indice 6 di `helloString` usando la notazione con parentesi quadre e memorizzalo in una variabile chiamata `charAtSix`.  
-4. Stampa `charAtSix` nella console (dovrebbe stampare "W").  
+🔸 ESERCIZIO 2: Accedi ai caratteri in una stringa  
+1. Crea una variabile chiamata `helloString` e assegnale la stringa esatta: 'Hello World'.  
+2. Accedi al carattere all'indice 6 della stringa `helloString` usando la notazione con parentesi quadre e memorizzalo in una variabile chiamata `charAtSix`.  
+3. Stampa `charAtSix` nella console (dovrebbe stampare "W").  
 
 Salva le modifiche, aggiorna la pagina ed esegui il codice per vedere l'output.  
 ====================================== */
+
+const cities = ['Rome', 'Paris', 'Berlin'];
+const firstCity = cities[0];
+const lastCity = cities[2];
+console.log(firstCity);
+console.log(lastCity);
+
+const helloString = 'Hello World';
+const charAtSix = helloString[6];
+console.log(charAtSix);
