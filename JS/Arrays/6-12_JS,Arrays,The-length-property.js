e34f3b0362a20f60851ff8e26530ceccf246d07a
Nataly <kolodina_melbyk@mail.ru>
2025-06-04 19:01:58 +0400

    Create 6-12_JS,Arrays,The-length-property.js

---
+++ JS/Arrays/6-12_JS,Arrays,The-length-property.js
@@ -0,0 +1,26 @@
+// 6-12_JS,Arrays,The-length-property
+
+/* ======================================
+🔸 Esercizio 1: Conta gli elementi in un array
+1. Crea un array `fruits` con const e assegnagli esattamente queste stringhe, in quest’ordine:
+   'apple', 'banana', 'cherry', 'date'.
+2. Usa console.log() per stampare la proprietà length dell’array `fruits`.
+   // Output atteso: 4
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per vedere il risultato.
+====================================== */
+
+/* ======================================
+🔸 Esercizio 2: Usa length per accedere all’ultimo elemento
+1. Crea un array `colors` con const e inserisci queste stringhe:
+   'red', 'green', 'blue', 'yellow', 'purple'.
+2. Usa la proprietà length per calcolare l’indice dell’ultimo elemento nell’array `colors`.
+3. Usa la notazione tra parentesi quadre per accedere all’ultimo elemento e stampalo nella console.
+   // Output atteso: 'purple'
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per vedere il risultato.
+====================================== */
+
+
+
+
