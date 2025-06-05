d0ce385f0201a983b27406409a2cac333fe88526
Nataly <kolodina_melbyk@mail.ru>
2025-06-05 15:17:06 +0400

    Create 8-10_JS,Loops,Do-While-Statements.js

---
+++ JS/Loops/8-10_JS,Loops,Do-While-Statements.js
@@ -0,0 +1,32 @@
+// 8-10_JS,Loops,Do-While-Statements
+
+/* ======================================
+ESERCIZIO 1: Stampa i numeri da 1 a 5 usando un ciclo do...while
+1. Dichiara una variabile chiamata counter e inizializzala a 1.
+2. Scrivi un ciclo do...while che:
+   - All’interno del blocco do, stampa il valore di counter nella console.
+   - Incrementa il valore di counter di 1 dopo la stampa.
+   - Continua il ciclo finché counter è minore o uguale a 5.
+Output atteso nella console:
+1
+2
+3
+4
+5
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per verificare l’output.
+====================================== */
+
+/* ======================================
+ESERCIZIO 2: Usa do...while per stampare almeno una volta un messaggio
+1. Dichiara una variabile chiamata keepRunning e inizializzala con valore false.
+2. Scrivi un ciclo do...while che:
+   - All’interno del blocco do, stampa nella console il testo "Esecuzione garantita".
+   - Continua il ciclo finché keepRunning è vero.
+Output atteso nella console:
+Esecuzione garantita
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per verificare l’output.
+====================================== */
+
+
