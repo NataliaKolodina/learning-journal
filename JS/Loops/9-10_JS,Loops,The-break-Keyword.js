2a636b1531c45b6ea7f5e0ee1d714b92c7bfd554
Nataly <kolodina_melbyk@mail.ru>
2025-06-05 15:27:19 +0400

    Create 9-10_JS,Loops,The-break-Keyword.js

---
+++ JS/Loops/9-10_JS,Loops,The-break-Keyword.js
@@ -0,0 +1,36 @@
+// 9-10_JS,Loops,The-break-Keyword
+
+/* ======================================
+ESERCIZIO 1: Usa break per interrompere un ciclo for
+1. Scrivi un ciclo for che inizializza una variabile i a 0, esegue il ciclo finché i è minore di 10, e incrementa i di 1 ad ogni iterazione.
+2. All’interno del ciclo, scrivi un'istruzione if che verifica se i è uguale a 4.
+3. Se i è uguale a 4, usa break per uscire dal ciclo.
+4. Altrimenti, stampa il valore di i nella console.
+Output atteso nella console:
+0
+1
+2
+3
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per verificare l’output.
+====================================== */
+
+/* ======================================
+ESERCIZIO 2: Interrompi un ciclo while usando break quando una condizione è soddisfatta
+1. Dichiara una variabile chiamata counter e inizializzala a 0.
+2. Scrivi un ciclo while che continua finché counter è minore di 10.
+3. All’interno del ciclo, se counter è uguale a 6, usa break per uscire dal ciclo.
+4. Se non esce, stampa il valore di counter nella console.
+5. Incrementa counter di 1 alla fine di ogni iterazione.
+Output atteso nella console:
+0
+1
+2
+3
+4
+5
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per verificare l’output.
+====================================== */
+
+
