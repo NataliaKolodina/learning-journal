ed24540bc3dd84cbc478b2d077b63777aa3b5315
Nataly <kolodina_melbyk@mail.ru>
2025-06-05 15:33:28 +0400

    Create 10-10_JS,Loops,Review.js

---
+++ JS/Loops/10-10_JS,Loops,Review.js
@@ -0,0 +1,34 @@
+// 10-10_JS,Loops,Review
+
+/* ======================================  
+Esercizio 1: Stampa i numeri da 10 a 6 con un ciclo for e break  
+1. Crea un ciclo for con una variabile chiamata `i` che parte da 10, continua finché `i` è maggiore o uguale a 1, decrementando `i` di 1 ad ogni iterazione.  
+2. All’interno del ciclo, scrivi un’istruzione if che interrompe il ciclo se `i` è minore di 6.  
+3. Se la condizione di break non è soddisfatta, stampa il valore di `i` nella console.  
+Output atteso nella console:  
+10  
+9  
+8  
+7  
+6  
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per verificare l’output.  
+====================================== */
+
+/* ======================================  
+Esercizio 2: Usa un ciclo while con break e contatore  
+1. Dichiara una variabile chiamata `counter` inizializzata a 3.  
+2. Scrivi un ciclo while che continua finché `counter` è minore di 10.  
+3. All’interno del ciclo, se `counter` è uguale a 7, usa break per uscire dal ciclo.  
+4. Altrimenti, stampa il valore di `counter` nella console.  
+5. Incrementa `counter` di 1 alla fine di ogni iterazione.  
+Output atteso nella console:  
+3  
+4  
+5  
+6  
+
+Salva le modifiche, aggiorna la pagina ed esegui il codice per verificare l’output.  
+====================================== */
+
+
