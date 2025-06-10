1aad19d46d837517886c41ae6c74f0d075fb6b9b
Nataly <kolodina_melbyk@mail.ru>
2025-06-10 17:02:21 +0400

    Create 2-4_JS,Higher-Order-Functions,Functions-as-Data.js

---
+++ JS/Higher-Order Functions/2-4_JS,Higher-Order-Functions,Functions-as-Data.js
@@ -0,0 +1,24 @@
+// 2-4_JS,Higher-Order-Functions,Functions-as-Data
+
+/* ======================================
+ESERCIZIO 1: Assegnazione e Invocazione di Funzioni
+
+1. Definisci una funzione freccia chiamata `showMessage` che stampa in console la frase "Sto lavorando!".
+2. Assegna la funzione `showMessage` a una nuova costante chiamata `work`.
+3. Chiama la funzione tramite la costante `work`.
+   // Risultato atteso in console: Sto lavorando!
+
+Salva il file, esegui il codice e verifica il risultato nella console.
+====================================== */
+
+/* ======================================
+ESERCIZIO 2: Proprietà delle Funzioni
+
+1. Usa la funzione `showMessage` definita nell'esercizio precedente.
+2. Stampa in console il nome della funzione usando la proprietà `.name`.
+   // Risultato atteso: showMessage
+3. Stampa in console la lunghezza (numero di parametri attesi) della funzione `showMessage` usando la proprietà `.length`.
+   // Risultato atteso: 0
+
+Salva il file, esegui il codice e verifica il risultato nella console.
+====================================== */
