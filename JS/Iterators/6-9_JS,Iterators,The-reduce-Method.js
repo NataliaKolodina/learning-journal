7d3af3aa9508d9aa3246220b74874f335d092493
Nataly <kolodina_melbyk@mail.ru>
2025-06-17 15:46:38 +0400

    Create 6-9_JS,Iterators,The-reduce-Method.js

---
+++ JS/Iterators/6-9_JS,Iterators,The-reduce-Method.js
@@ -0,0 +1,26 @@
+// 6-9_JS,Iterators,The-reduce-Method
+/* ======================================
+ESERCIZIO 1: Sommare tutti i numeri di un array
+
+1. Crea un array chiamato `prices` con i seguenti valori: 10, 20, 30, 40.
+2. Crea una costante `total` e assegna il risultato della chiamata a `.reduce()` su `prices`.
+3. La funzione freccia deve avere due parametri: `accumulator` e `currentValue`.
+4. La funzione deve restituire la somma tra `accumulator` e `currentValue`.
+   // Risultato atteso per total: 100
+5. Stampa `total` nella console.
+
+Salva il file, esegui il codice e verifica l'output.
+====================================== */
+
+/* ======================================
+ESERCIZIO 2: Ridurre un array con un valore iniziale
+
+1. Crea un array chiamato `bonusPoints` con i seguenti valori: 5, 10, 15.
+2. Crea una costante `finalScore` e assegna il risultato della chiamata a `.reduce()` su `bonusPoints`.
+3. La funzione freccia deve restituire la somma tra `accumulator` e `currentValue`.
+4. Passa anche 50 come secondo argomento a `.reduce()` per inizializzare l’accumulatore.
+   // Risultato atteso per finalScore: 80
+5. Stampa `finalScore` nella console.
+
+Salva il file, esegui il codice e verifica l'output.
+====================================== */
