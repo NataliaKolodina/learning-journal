c61066a11cfcefeeef1e16e40d974edd68dc8508
Nataly <kolodina_melbyk@mail.ru>
2025-06-17 15:50:01 +0400

    Update 6-9_JS,Iterators,The-reduce-Method.js

--- JS/Iterators/6-9_JS,Iterators,The-reduce-Method.js
+++ JS/Iterators/6-9_JS,Iterators,The-reduce-Method.js
@@ -24,3 +24,15 @@ ESERCIZIO 2: Ridurre un array con un valore iniziale
 
 Salva il file, esegui il codice e verifica l'output.
 ====================================== */
+
+const prices = [10, 20, 30, 40];
+const total = prices.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+});
+console.log(total);
+
+const bonusPoints = [5, 10, 15];
+const finalScore = bonusPoints.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 50);
+console.log(finalScore);
