f3901392e4e40881a21da1260bbba37ff3da16f5
Nataly <kolodina_melbyk@mail.ru>
2025-06-24 16:51:13 +0400

    Create 6-10_JS,Objects,Methods.js

---
+++ JS/Objects/6-10_JS,Objects,Methods.js
@@ -0,0 +1,23 @@
+// 6-10_JS,Objects,Methods
+/* ======================================
+ESERCIZIO: Creare e invocare un metodo su un oggetto
+
+1. Crea una costante `securityBot` e assegnale un oggetto con le seguenti proprietà:
+   - `model`: con valore `'Guardian-X'`
+   - `alert`: un metodo che stampa il messaggio `"Intruder detected! Activating defense mode."` usando `console.log()`.
+2. Invoca il metodo `alert` dell’oggetto `securityBot`.
+   // Risultato atteso nella console:
+   // "Intruder detected! Activating defense mode."
+====================================== */
+
+/* ======================================
+ESERCIZIO: Aggiungere un metodo a un oggetto esistente
+
+1. Crea una costante `drone` e assegnale un oggetto con la proprietà:
+   - `id`: con valore `'DR-22'`
+2. Aggiungi una nuova proprietà chiamata `fly` a `drone`, assegnandole un metodo (utilizzando la sintassi ES6) che stampa il messaggio `"Drone in flight. ID: DR-22"` usando `console.log()`.
+3. Invoca il metodo `fly` dell’oggetto `drone`.
+   // Risultato atteso nella console:
+   // "Drone in flight. ID: DR-22"
+====================================== */
+
