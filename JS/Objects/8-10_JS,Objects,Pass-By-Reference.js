36310d68d09f8ed7c68239565fe528a411a0e2ce
Nataly <kolodina_melbyk@mail.ru>
2025-06-24 17:21:22 +0400

    Update 8-10_JS,Objects,Pass-By-Reference.js

--- JS/Objects/8-10_JS,Objects,Pass-By-Reference.js
+++ JS/Objects/8-10_JS,Objects,Pass-By-Reference.js
@@ -32,4 +32,26 @@ ESERCIZIO: Tentativo di riassegnazione di oggetto all'interno di una funzione
    // Dopo la funzione: { type: "truck", inUse: true }
 ====================================== */
 
+const robot = {
+  status: "offline",
+  battery: 15,
+};
+let activateRobot = (machine) => {
+  machine.status = "active";
+};
+activateRobot(robot);
+console.log(robot.status);
 
+let vehicle = {
+  type: "truck",
+  inUse: true,
+};
+let replaceVehicle = (machine) => {
+  machine = {
+    type: "drone",
+    inUse: false,
+  };
+  console.log(machine);
+};
+replaceVehicle(vehicle);
+console.log(vehicle);
