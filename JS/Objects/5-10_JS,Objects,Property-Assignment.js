99ef2c93dd88649bdada6c8c53d399118997ecd9
Nataly <kolodina_melbyk@mail.ru>
2025-06-24 16:44:32 +0400

    Create 5-10_JS,Objects,Property-Assignment.js

---
+++ JS/Objects/5-10_JS,Objects,Property-Assignment.js
@@ -0,0 +1,27 @@
+// 5-10_JS,Objects,Property-Assignment
+/* ======================================
+ESERCIZIO: Assegnare e aggiornare proprietà di un oggetto
+
+1. Crea una costante `robot` e assegnale un oggetto con una sola proprietà:
+   - `model`: con valore `'RX-78'`
+2. Utilizzando la notazione con il punto, cambia il valore della proprietà `model` in `'RX-79'`.
+3. Utilizzando la notazione con parentesi quadre, aggiungi una nuova proprietà `'energy Level'` con valore `100`.
+4. Crea una costante `energy` e assegnale il valore della proprietà `'energy Level'` dell’oggetto `robot` usando la notazione con parentesi quadre.
+5. Stampa `energy` usando `console.log()`.
+   // Risultato atteso nella console:
+   // 100
+====================================== */
+
+/* ======================================
+ESERCIZIO: Eliminare una proprietà da un oggetto
+
+1. Crea una costante `car` e assegnale un oggetto con le seguenti proprietà:
+   - `brand`: con valore `'Fiat'`
+   - `color`: con valore `'red'`
+   - `year`: con valore `2010`
+2. Crea una costante `manufactureYear` e assegnale il valore della proprietà `year` dell’oggetto `car`.
+3. Elimina la proprietà `year` usando l’operatore `delete`.
+4. Stampa `manufactureYear` e `year`  usando `console.log()`.
+   // Risultato atteso nella console:
+   // 2010 undefined
+
