aef7e06e8bc3defc22e457fa669f80a3d786d4f1
Nataly <kolodina_melbyk@mail.ru>
2025-06-26 23:04:37 +0400

    Update 5-11_JS,Advanced-Objects,Getters.js

--- JS/Advanced Objects/5-11_JS,Advanced-Objects,Getters.js
+++ JS/Advanced Objects/5-11_JS,Advanced-Objects,Getters.js
@@ -17,4 +17,31 @@ ESERCIZIO 2: Getter con controllo sulla lunghezza
 5. Esegui di nuovo `book.shortTitle`. // Risultato atteso: Dune
 ====================================== */
 
+const employee = {
+  _firstName: "Sara",
+  _lastName: "Rossi",
+  get fullName() {
+    if (this._firstName && this._lastName) {
+      return `${this._firstName} ${this._lastName}`;
+    } else {
+      return "Nome o cognome mancante.";
+    }
+  },
+};
+console.log(employee.fullName);
+delete employee._lastName;
+console.log(employee.fullName);
 
+const book = {
+  _title: "Il Signore degli Anelli",
+  get shortTitle() {
+    if (this._title.length > 20) {
+      return "Titolo troppo lungo.";
+    } else {
+      return this._title;
+    }
+  },
+};
+console.log(book.shortTitle);
+book._title = "Dune";
+console.log(book.shortTitle);
